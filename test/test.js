7ba1de259b609c2b5e1418245ba22dc793f04408
NullDev (Shadow) <NL-Dev@yandex.com>
2017-12-15 11:03:12 +0100

    Added bosnian test

--- test/test.js
+++ test/test.js
@@ -43,3 +43,23 @@ console.log("-902.09 = " + nummern(-902.09, "german"));
 console.log("100000  = " + nummern(100000,  "german"));
 
 console.log();
+
+/**
+ * Bosnian
+ */
+
+console.log("1234    = " + nummern(1234,    "bosnian"));
+
+console.log("-100    = " + nummern(-100,    "bosnian"));
+
+console.log("122.045 = " + nummern(122.045, "bosnian"));
+
+console.log("-1      = " + nummern(-1,      "bosnian"));
+
+console.log("1000000 = " + nummern(1000000, "bosnian"));
+
+console.log("-902.09 = " + nummern(-902.09, "bosnian"));
+
+console.log("100000  = " + nummern(100000,  "bosnian"));
+
+console.log();
